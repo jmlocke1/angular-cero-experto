b98e4bdfba7fe21fbf6d9e76feddde7aaba4cc25
Josemi <worldcode.info@gmail.com>
2026-03-19 21:38:40 +0100

    Importaciones y Exportaciones

--- 01-typescript-intro/src/topics/06-function-destructuring.ts
+++ 01-typescript-intro/src/topics/06-function-destructuring.ts
@@ -1,18 +1,18 @@
 
-interface Product {
+export interface Product {
     description: string;
     price: number;
 }
 
-const phone: Product = {
-    description: 'Nokia A1',
-    price: 150.0
-}
+// const phone: Product = {
+//     description: 'Nokia A1',
+//     price: 150.0
+// }
 
-const tablet: Product = {
-    description: 'iPad Air',
-    price: 250.0
-}
+// const tablet: Product = {
+//     description: 'iPad Air',
+//     price: 250.0
+// }
 
 interface TaxCalculationOptions {
     tax: number;
@@ -21,7 +21,7 @@ interface TaxCalculationOptions {
 
 // Aquí hay dos formas de hacerlo, pero las opciones pueden tener muchas propiedades, por lo que es preferible la segunda
 // function taxCalculation( { products, tax: taxOption }: TaxCalculationOptions ): [number, number] {
-function taxCalculation( options: TaxCalculationOptions ): [number, number] {
+export function taxCalculation( options: TaxCalculationOptions ): [number, number] {
     let total = 0;
     const { products, tax } = options;
     products.forEach( ({ price }) => {
@@ -32,15 +32,15 @@ function taxCalculation( options: TaxCalculationOptions ): [number, number] {
     return [ total, total * tax ];
 }
 
-const shoppingCart = [phone, tablet];
-const tax = 0.15;
+// const shoppingCart = [phone, tablet];
+// const tax = 0.15;
 
-const [ total, taxProducts ] = taxCalculation({
-    products: shoppingCart,
-    tax
-});
+// const [ total, taxProducts ] = taxCalculation({
+//     products: shoppingCart,
+//     tax
+// });
 
-console.log('Total:', total);
-console.log('Tax:', taxProducts);
+// console.log('Total:', total);
+// console.log('Tax:', taxProducts);
 
-export {};
+// export { type Product};
