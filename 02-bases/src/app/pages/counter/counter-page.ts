3ba9bdbe8b7f7defd22bab006b52b09bccfdfb87
Josemi <worldcode.info@gmail.com>
2026-03-22 00:32:18 +0100

    Comienzo con señales

--- 02-bases/src/app/pages/counter/counter-page.ts
+++ 02-bases/src/app/pages/counter/counter-page.ts
@@ -1,20 +1,32 @@
-import { Component } from "@angular/core";
+import { Component, signal, ChangeDetectionStrategy } from "@angular/core";
 
 
 
 
 @Component({
     templateUrl: './counter-page.html',
-    styleUrl: './counter-page.css'
+    styleUrl: './counter-page.css',
+    // changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CounterPage {
     counter = 10;
+    counterSignal = signal(10);
+
+    constructor() {
+        setInterval(() => {
+            this.counter += 1;
+            this.counterSignal.update( v => v + 1 );
+            console.log('Tick');
+        }, 2000);
+    }
 
     increaseBy(value: number) {
         this.counter += value;
+        // this.counterSignal.update( current => current + value)
     }
 
     reset() {
-        this.counter = 10;
+        this.counter = 0;
+        this.counterSignal.set(0);
     }
 }
