a663554689f9a0faea04f75c80f1593c96f6fbcd
Josemi <worldcode.info@gmail.com>
2026-03-24 00:17:45 +0100

    RouterLink y RouterLinkActive

--- 02-bases/src/app/app.routes.ts
+++ 02-bases/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { CounterPage } from './pages/counter/counter-page';
 import { HeroPage } from './pages/hero/hero-page';
+import { DragonballPage } from './pages/dragonball/dragonball-page';
 
 export const routes: Routes = [
 
@@ -12,4 +13,12 @@ export const routes: Routes = [
         path: 'hero',
         component: HeroPage
     },
+    {
+        path: 'dragonball',
+        component: DragonballPage
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
 ];
---
+++ 02-bases/src/app/pages/dragonball/dragonball-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dragonball-page',
+  imports: [],
+  templateUrl: './dragonball-page.html',
+  styleUrl: './dragonball-page.css',
+})
+export class DragonballPage {}
