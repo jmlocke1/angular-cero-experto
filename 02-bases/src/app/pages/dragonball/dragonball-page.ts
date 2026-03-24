13c5d3c3aa109caf1456e4749106367d89de23de
Josemi <worldcode.info@gmail.com>
2026-03-24 22:43:35 +0100

    Iteraciones de elementos y ngClass

--- 02-bases/src/app/pages/dragonball/dragonball-page.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, signal, computed } from '@angular/core';
+
+
+interface Character {
+    id: number;
+    name: string;
+    power: number;
+}
 
 @Component({
-  selector: 'app-dragonball-page',
-  imports: [],
-  templateUrl: './dragonball-page.html',
-  styleUrl: './dragonball-page.css',
+    selector: 'app-dragonball-page',
+    imports: [NgClass],
+    templateUrl: './dragonball-page.html',
+    styleUrl: './dragonball-page.css',
 })
-export class DragonballPage {}
+export class DragonballPage {
+
+    characters = signal<Character[]>([
+        { id: 1, name: 'Goku', power: 9001 },
+        { id: 2, name: 'Vegeta', power: 9000 },
+        { id: 3, name: 'Piccolo', power: 3000 },
+    ]);
+
+    // powerClasses = computed( () => {
+    //     return {
+    //         'text-danger': true,
+    //     }
+    // });
+}
